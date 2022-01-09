5a0653787a92e4ad6271c46e3a301ef4515aee62
amachkel <amachkel@gmail.com>
2022-01-09 17:11:00 -0500

    Edited navbar in mobile to center text with a width of 100%.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*hamburger menu for mobile*/
 function myFunction() {
     var x = document.getElementById("navbar");
+    x.classList.toggle('active');
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
