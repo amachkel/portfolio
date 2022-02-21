bf39cbd3b3fa35eb48c6632e97bbdb57fd20d29f
amachkel <amachkel@gmail.com>
2022-02-21 12:39:28 -0500

    Reorganized files. Added resume pdf link.

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// hamburger menu for mobile
+function myHamburger() {
+    var x = document.getElementById("navbar");
+    x.classList.toggle('active');
+}
+// stops navbar from showing when reloaded
