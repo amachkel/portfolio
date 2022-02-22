3428cbaad4c0570cfba4ef661b20ea8bd78d8258
amachkel <amachkel@gmail.com>
2022-02-22 11:55:47 -0500

    Added and styled tile-content text

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-// hamburger menu for mobile
-function myHamburger() {
-    var x = document.getElementById("navbar");
-    x.classList.toggle('active');
-}
-// stops navbar from showing when reloaded
